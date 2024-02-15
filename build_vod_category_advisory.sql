79c0530425b824cee92e83da5cdde1b1f0455cdb
Brooke Johnson <147000783+brookej01@users.noreply.github.com>
2024-02-15 14:11:10 -0700

    Update build_vod_category_advisory.sql

--- build_vod_category_advisory.sql
+++ build_vod_category_advisory.sql
@@ -8,7 +8,7 @@ CREATE TABLE vod_category (
     category_id NUMBER,
     category_name VARCHAR2(15) CONSTRAINT vod_cat_name_nn NOT NULL,
     parent_category_id NUMBER CONSTRAINT parent_cat_fk REFERENCES vod_category(category_id),
-    CONSTRAINT vod_category_pk PRIMARY KEY (category_id) -- Changed constraint name
+    CONSTRAINT vod_category_pk PRIMARY KEY (category_id)
 );
 
 -- Creating the ADVISORY table
@@ -17,5 +17,5 @@ CREATE TABLE vod_advisory (
     advisory_type NUMBER(2) NOT NULL CONSTRAINT adv_type_ck CHECK (advisory_type BETWEEN 1 AND 17),
     short_description VARCHAR2(255) NOT NULL,
     full_description VARCHAR2(1000) NOT NULL,
-    CONSTRAINT vod_advisory_pk PRIMARY KEY (advisory_id) -- Changed constraint name
+    CONSTRAINT vod_advisory_pk PRIMARY KEY (advisory_id) 
 );
