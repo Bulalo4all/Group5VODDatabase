18a6b9a90de875afe4471f863fc4f92341fdf34e
Brooke Johnson <147000783+brookej01@users.noreply.github.com>
2024-02-15 15:05:25 -0700

    add Category and Advisory bridges

--- build_vod_category_advisory.sql
+++ build_vod_category_advisory.sql
@@ -23,3 +23,27 @@ CREATE TABLE vod_advisory (
     short_description VARCHAR2(255) NOT NULL,
     full_description VARCHAR2(1000) NOT NULL
 );
+
+-- Creating the VOD_MOVIE_ADVISORY_BRIDGE
+CREATE TABLE VOD_MOVIE_ADVISORY_BRIDGE (
+    movie_advisory_bridge_id NUMBER
+        CONSTRAINT sys_movie_advisory_bridge_id_PK PRIMARY KEY,
+    advisory_id NUMBER
+        CONSTRAINT sys_advisory_id_movie_bridge_FK REFERENCES vod_advisory(advisory_id),
+    movie_id NUMBER
+        CONSTRAINT sys_movie_id_advisory_bridge_FK REFERENCES vod_movie(movie_id)
+
+
+);
+
+-- Creating the VOD_MOVIE_CATEGORY_BRIDGE
+CREATE TABLE VOD_MOVIE_CATEGORY_BRIDGE (
+    movie_category_bridge_id NUMBER
+        CONSTRAINT sys_movie_category_bridge_id_PK PRIMARY KEY,
+    category_id NUMBER
+        CONSTRAINT sys_category_id_movie_bridge_FK REFERENCES vod_category(category_id),
+    movie_id NUMBER
+        CONSTRAINT sys_movie_id_category_bridge_FK REFERENCES vod_movie(movie_id)
+
+
+);
